2627a79558de799111396dd9cbf51d365933b061
Marcus Baker <marcus@conversocial.com>
2020-05-29 13:13:52 +0100

    last thing on holiday

--- bwt.c
+++ bwt.c
@@ -3,7 +3,17 @@
 #include "rotation.h"
 #include "bwt.h"
 
+struct SuffixList_ {
+    char **grid;
+    unsigned int length;
+};
+typedef struct SuffixList_ SuffixList;
+
 int compare_rotation_pointer(const void *rotation_p1, const void *rotation_p2);
+SuffixList *create_suffix_list(unsigned int length);
+void free_suffix_list(SuffixList *suffixes);
+void populate_column(SuffixList *suffixes, int offset, char *characters);
+int compare_suffix(void *suffix1, void *suffix2, void *_offset);
 
 char *bwt(char *text) {
     int length = strlen(text);
@@ -25,7 +35,9 @@ char *bwt(char *text) {
 }
 
 char *reverse_bwt(char *bwt) {
-    char grid[strlen(bwt)][strlen(bwt) + 1];
+    SuffixList *suffixes = create_suffix_list(strlen(bwt));
+    populate_column(suffixes, strlen(bwt) - 1, bwt);
+    free_suffix_list(suffixes);
     return strdup("(not implemented)");
 }
 
@@ -33,3 +45,29 @@ int compare_rotation_pointer(const void *rotation_p1, const void *rotation_p2) {
     return compare_rotation(*(Rotation **)rotation_p1, *(Rotation **)rotation_p2);
 }
 
+SuffixList *create_suffix_list(unsigned int length) {
+    SuffixList *suffixes = (SuffixList *)malloc(sizeof(SuffixList));
+    suffixes->length = length;
+    for (int i = 0; i < suffixes->length; i++) {
+        suffixes->grid[i] = (char *)malloc((suffixes->length + 1) * sizeof(char));
+        suffixes->grid[i][suffixes->length] = '\0';
+    }
+    return suffixes;
+}
+
+void free_suffix_list(SuffixList *suffixes) {
+    for (int i = 0; i < suffixes->length; i++) {
+        free(suffixes->grid[i]);
+    }
+    free(suffixes);
+}
+
+void populate_column(SuffixList *suffixes, int offset, char *characters) {
+    for (int i = 0; i < suffixes->length; i++) {
+        suffixes->grid[i][offset] = characters[i];
+    }
+}
+
+int compare_suffix(void *suffix1, void *suffix2, void *offset) {
+    return strcmp(suffix1 + (int)offset, suffix2 + (int)offset);
+}
